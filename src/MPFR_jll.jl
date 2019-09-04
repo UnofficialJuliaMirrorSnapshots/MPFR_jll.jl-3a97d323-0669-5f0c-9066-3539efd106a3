94ff7047a2283face8a7da015b2b49f2c52b1ac3
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-04 13:24:47 +0000

    Snapshot of branch master taken on 2019-09-04T09:24:46.463-04:00 from "https://github.com/UnofficialJuliaMirror/MPFR_jll.jl-3a97d323-0669-5f0c-9066-3539efd106a3"

---
+++ src/MPFR_jll.jl
@@ -0,0 +1,20 @@
+module MPFR_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml, UUID("45fb39a0-e3b2-5699-9cbf-fcdff2f9388e"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "MPFR", artifacts_toml) for e in artifacts["MPFR"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p))), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Load the appropriate wrapper
+include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+
+end  # module MPFR_jll
